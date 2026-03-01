49c2a182c01e2f12830f1bfead15a0c47c040bf6
HiteshChugh-2006 <hiteshchugh101@gmail.com>
2026-03-01 18:52:39 +0530

    fixed duplicate middleware

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 import dotenv from "dotenv";
 dotenv.config();
+
 import express from "express";
-import {connectDB}from"./config/db.js";
+import { connectDB } from "./config/db.js";
 import cardRoutes from "./routes/cardRoutes.js";
 
 const app = express();
 
+// Middleware
 app.use(express.json());
+
+// Root Route
+app.get("/", (req, res) => {
+  res.send("🚀 Playing Cards API is running successfully on Render!");
+});
+
+// API Routes
 app.use("/api/cards", cardRoutes);
+
+// Connect DB
 connectDB();
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
